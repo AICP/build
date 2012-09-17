a7f0b0d117cbc018897ebc281b3a62dddaa14617
Steve Kondik <shade@chemlab.org>
2012-09-17 11:33:18 -0700

    build: Add "aospremote" command
     * Adds the git remote for the matching AOSP repository.
    
    Change-Id: Iad03fb95874ff39a39083218aec0d834e82b4480

--- envsetup.sh
+++ envsetup.sh
@@ -19,6 +19,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - resgrep: Greps on all local res/*.xml files.
 - sgrep:   Greps on all local source files.
 - godir:   Go to the directory containing a file.
+- aospremote: Add git remote for matching AOSP repository
 - mka:      Builds using SCHED_BATCH on all processors
 - reposync: Parallel repo sync using ionice and SCHED_BATCH
 
@@ -1538,6 +1539,23 @@ function godir () {
     fi
     \cd $T/$pathname
 }
+
+function aospremote()
+{
+    git remote rm aosp 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
+    if (echo $PROJECT | grep -qv "^device")
+    then
+        PFX="platform/"
+    fi
+    git remote add aosp https://android.googlesource.com/$PFX$PROJECT
+    echo "Remote 'aosp' created"
+}
+export -f aospremote
 
 function repodiff() {
     if [ -z "$*" ]; then
