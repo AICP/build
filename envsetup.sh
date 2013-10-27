cef57d10bedced727cf578d576165ee37226e9c8
Steve Kondik <shade@chemlab.org>
2013-10-27 13:34:36 -0700

    build: Add cafremote command to envsetup
     * So lazy!
    
    Change-Id: I99a988180abb4fb486d4ebbb842bb9eda03bf1bc

--- envsetup.sh
+++ envsetup.sh
@@ -20,6 +20,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sgrep:   Greps on all local source files.
 - godir:   Go to the directory containing a file.
 - aospremote: Add git remote for matching AOSP repository
+- cafremote: Add git remote for matching CodeAurora repository.
 - mka:      Builds using SCHED_BATCH on all processors
 - reposync: Parallel repo sync using ionice and SCHED_BATCH
 
@@ -1571,6 +1572,23 @@ function aospremote()
     echo "Remote 'aosp' created"
 }
 export -f aospremote
+
+function cafremote()
+{
+    git remote rm caf 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`pwd | sed s#$ANDROID_BUILD_TOP/##g`
+    if (echo $PROJECT | grep -qv "^device")
+    then
+        PFX="platform/"
+    fi
+    git remote add caf git://codeaurora.org/$PFX$PROJECT
+    echo "Remote 'caf' created"
+}
+export -f cafremote
 
 function repodiff() {
     if [ -z "$*" ]; then
