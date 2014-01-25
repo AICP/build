557572b70889bd8cac8e65d0429a725a106e42ae
Anthony King <anthonydking@gmail.com>
2014-01-25 17:29:20 +0000

    build: use the system's ccache by default
    ccache 3.x supports direct mode (http://ccache.samba.org/manual.html#_the_direct_mode)
    This is not supported in the prebuilt version.
    Direct mode has been shown to decrease build time compared to the ccache in prebuilts
    
    Note: this will require your cache to be recreated
    
    PS3:
      make it so you need to export USE_SYSTEM_CCACHE=1 to use system ccache
      allow custom CCACHE_BASEDIR
    oldChange-Id: Ib146011a3330637c2d306d329fcb39cea058ff12
    
    Conflicts:
    	core/combo/select.mk
    
    Change-Id: Iee698ee4caf46a73986b748dc38709023416da1f

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -86,19 +86,33 @@ ifneq ($(USE_CCACHE),)
   # We don't really use system headers much so the rootdir is
   # fine; ensures these paths are relative for all Android trees
   # on a workstation.
-  export CCACHE_BASEDIR := /
+  ifeq ($(CCACHE_BASEDIR),)
+    export CCACHE_BASEDIR := /
+  endif
 
   # Workaround for ccache with clang.
   # See http://petereisentraut.blogspot.com/2011/09/ccache-and-clang-part-2.html
   export CCACHE_CPP2 := true
 
-  CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
-  # If we are cross-compiling Windows binaries on Linux
-  # then use the linux ccache binary instead.
-  ifeq ($(HOST_OS)-$(BUILD_OS),windows-linux)
-    CCACHE_HOST_TAG := linux-$(HOST_PREBUILT_ARCH)
+  # It has been shown that ccache 3.x using direct mode can be several times
+  # faster than using the current ccache 2.4 that is used by default
+  # use the system ccache if asked to, else default to the one in prebuilts
+
+  ifeq ($(USE_SYSTEM_CCACHE),)
+    ccache :=
+  else
+    ccache := $(shell which ccache)
   endif
-  ccache := prebuilts/misc/$(CCACHE_HOST_TAG)/ccache/ccache
+
+  ifeq ($(ccache),)
+    CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
+    # If we are cross-compiling Windows binaries on Linux
+    # then use the linux ccache binary instead.
+    ifeq ($(HOST_OS)-$(BUILD_OS),windows-linux)
+      CCACHE_HOST_TAG := linux-$(HOST_PREBUILT_ARCH)
+    endif
+    ccache := prebuilts/misc/$(CCACHE_HOST_TAG)/ccache/ccache
+  endif
   # Check that the executable is here.
   ccache := $(strip $(wildcard $(ccache)))
   ifdef ccache
