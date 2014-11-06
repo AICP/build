68e4ec02f15e32453d97d20294fdec8f4f17b79f
Anthony King <anthonydking@gmail.com>
2014-11-06 20:19:44 +0000

    clang: substitute fpu neon-vfp[3|4] for fpu neon
    Clang does not natively support the neon-vfp option
    so we will substitute it for neon.
    This allows neon-vfp to be set for gcc, and won't cause
    clang to error out.
    
    Clang will enable neon and a vfp automatically with supported
    cpu's.
    Do not set fpu to vfp[3|4] as this may remove neon
    
    Change-Id: I21615d4bd156d24f4e4870c1021c46e74e0d3389

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -28,5 +28,9 @@ define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
   $(subst -mcpu=cortex-a15,-march=armv7-a,\
-  $(1))))
+  $(subst -mfpu=neon-vfp3,-mfpu=neon,\
+  $(subst -mfpu=neon-vfpv3,-mfpu=neon,\
+  $(subst -mfpu=neon-vfp4,-mfpu=neon,\
+  $(subst -mfpu=neon-vfpv4,-mfpu=neon,\
+  $(1))))))))
 endef
