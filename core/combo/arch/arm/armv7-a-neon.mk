4de9acdfbadf360997b6a4ec8eca774498388893
LorDClockaN <davor@losinj.com>
2014-11-06 22:01:50 +0100

    Revert "Revert "Apply correct, optimized mfpu compiler flag for ARMv7-A Cortex CPUs""
    This reverts commit 5adc088af0fc8e90a578795283050a4d5d985432.
    
    Change-Id: I5d07df56838ec380fb0ffc3ac5bc079ee5d519d0

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,21 +7,21 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a75
-	arch_variant_cflags := -mcpu=cortex-a5
+	arch_variant_cflags := -mcpu=cortex-a5 -mfpu=neon-vfpv4
 else
-	arch_variant_cflags := -march=armv7-a
+	arch_variant_cflags := -march=armv7-a -mfpu=neon
 endif
 endif
 endif
@@ -29,8 +29,7 @@ endif
 endif
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
+    -mfloat-abi=softfp
 
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
