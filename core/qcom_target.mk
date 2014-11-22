04246f32d454358c5d0f71f0b4c1e7c230265296
doc <doc.divxm@gmail.com>
2014-11-22 13:34:18 +0200

    build: Fix mismerge
    There are missed lines after following commit:
    
    https://github.com/AICP/build/commit/6bc584ba1a3acc506103bf4c4306ec51e0e82cd1
    
    Change-Id: Ifd338ff764a35a07a94856f9fa5409a538bcaae3

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -19,8 +19,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     # Enable DirectTrack for legacy targets
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
+    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
         TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
         TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
+    endif
     endif
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
