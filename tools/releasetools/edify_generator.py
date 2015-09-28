c55f0c345c1bd2541c6ca33a00ca2f52616352a6
Tom Marshall <tdm@cyngn.com>
2015-09-28 22:01:02 +0200

    [1/3] Install persistence support
    Copy files from OUT/install to ZIP/install and extract to /tmp/install
    at install time.  This is an extensible framework that may be used for
    other install tasks as needed.  Note that none of the install files are
    only extracted to a ramdisk, they are never written to /system.
    
    Run script persist.sh with argument "save" prior to formatting /system
    and again with argument "restore" after /system is installed.  This may
    be explicitly disabled by setting "TARGET_DISABLE_PERSIST := true".
    
    Change-Id: I8b3b298e3c43b31aa275c3cb5d2eeae4cd33760b

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -149,6 +149,9 @@ class EdifyGenerator(object):
   def RunBackup(self, command):
     self.script.append(('run_program("/tmp/install/bin/backuptool.sh", "%s");' % command))
 
+  def RunPersist(self, arg):
+    self.script.append('run_program("/tmp/install/bin/persist.sh", "%s");' % arg)
+
   def ValidateSignatures(self, command):
     self.script.append('package_extract_file("META-INF/org/cyanogenmod/releasekey", "/tmp/releasekey");')
     # Exit code 124 == abort. run_program returns raw, so left-shift 8bit
