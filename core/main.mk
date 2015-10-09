41ed9c6138c5c39dd5c6951e7d924cb0b3743079
LorDClockaN <davor@losinj.com>
2015-10-09 23:19:39 +0200

    Allow using other java than openjdk on linux
    Change-Id: I59bef8b0c59da841b534ea91bbda2c5d5b9f8094

--- core/main.mk
+++ core/main.mk
@@ -180,7 +180,7 @@ endif
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
 requires_openjdk := false
 ifeq ($(HOST_OS), linux)
-requires_openjdk := true
+requires_openjdk := false
 endif
 
 
