f9c9004edf4c2a2cf14271db67fc3632c2f22f17
doc HD <doc.divxm@gmail.com>
2015-12-10 22:16:39 +0200

    build: Use Google Nexus Audio Files [2/2]
    Conflicts:
    	target/product/core_base.mk
    	target/product/full_base.mk
    
    Change-Id: I11528ac9c838ebef14c889f13f34dda458b2f7a4

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -16,6 +16,10 @@
 # Note that components added here will be also shared in PDK. Components
 # that should not be in PDK should be added in lower level like core.mk.
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.notification_sound=Tethys.ogg \
+    ro.config.alarm_alert=Oxygen.ogg
+
 PRODUCT_PACKAGES += \
     ContactsProvider \
     DefaultContainerService \
--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -23,11 +23,16 @@ PRODUCT_PACKAGES := \
     libfwdlockengine \
     WAPPushManager
 
+# Additional settings used in all AOSP builds
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Tethys.ogg
+
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
