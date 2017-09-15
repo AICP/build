fd1d7e6b9fd76b778b2e6d609b22c63e09bcd3bd
Miccia <bono.michele94@gmail.com>
2017-09-15 12:05:24 +0200

    build: aicpfy
    Change-Id: I0a817ee550d869199a10db924b5d41c902377962

--- core/config.mk
+++ core/config.mk
@@ -188,7 +188,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
 
 -include vendor/extra/BoardConfigExtra.mk
--include vendor/lineage/config/BoardConfigLineage.mk
+-include vendor/aicp/config/BoardConfigAicp.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
@@ -966,10 +966,10 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(AICP_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/aicp/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
@@ -978,10 +978,10 @@ $(eval include device/lineage/sepolicy/common/sepolicy.mk)
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
 # Rules for QCOM targets
--include $(TOPDIR)vendor/lineage/build/core/qcom_target.mk
+-include $(TOPDIR)vendor/aicp/build/core/qcom_target.mk
 
 # Rules for MTK targets
--include $(TOPDIR)vendor/lineage/build/core/mtk_target.mk
+-include $(TOPDIR)vendor/aicp/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A AICP build needs only the AICP product makefiles.
+ifneq ($(AICP_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(AICP_BUILD)/aicp.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # AICP_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(AICP_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
