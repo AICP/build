0f4a87ac5f3d50ae14e5e14cc1bc5167d8a0b815
Ali B <abittin@gmail.com>
2017-09-20 09:34:27 +0300

    build: add maintainers to build.prop
    Change-Id: I9dc1c6a95c441829b5900ff9c83aa32540ddabf1

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -59,6 +59,9 @@ echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
 if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
+if [ -n "$DEVICE_MAINTAINERS" ] ; then
+  echo "ro.aicp.maintainer=$DEVICE_MAINTAINERS"
+fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
 echo "ro.gzosp.device=$GZOSP_DEVICE"
