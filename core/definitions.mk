84aea1d9532e2e2d0ba5ad6e616cd124aa367620
Sergey Vlasov <sigprof@gmail.com>
2018-01-08 04:30:10 -0500

    [squash] [dnm] improve low ram builds with Jack
    commit eab0befc420dc0d4a5262a558f5bbd139767498d (HEAD)
    Author: Sergey Vlasov <sigprof@gmail.com>
    Date:   Wed Dec 7 08:40:46 2016 +0300
    
        build: Invoke jack-admin server-gc after every build
    
        Change-Id: Ie9980fc3df4a60137e12129300579a266e049a1b
        Signed-off-by: Sergey Vlasov <sigprof@gmail.com>
    
    commit 894a8427e7dde4fec4576b0bdfa0f7f993d15113
    Author: Sergey Vlasov <sigprof@gmail.com>
    Date:   Tue Dec 6 10:38:18 2016 +0300
    
        build: Add exclusive lock to jack calls
    
        Parallel jack calls consume excessive amount of memory; add exclusive
        locking to avoid overlap with other jack or javadoc calls.
    
        Change-Id: I836b5abb0ef74aa7b8e0efd878a8806359c95775
        Signed-off-by: Sergey Vlasov <sigprof@gmail.com>
    
    commit fb0de8b40737b801c9d8fd6ac8e5d73167d33a60
    Author: Sergey Vlasov <sigprof@gmail.com>
    Date:   Tue Dec 6 09:19:33 2016 +0300
    
        build: Add exclusive lock to javadoc calls
    
        Running multiple javadoc processes in parallel consumes too much RAM;
        serialize those processes using flock(1).
    
        Change-Id: Ic41ea65368d36611ffc581dd72aced077c9458fb
        Signed-off-by: Sergey Vlasov <sigprof@gmail.com>
    
    Change-Id: Iea59d3fd45576bea5c40c6a7f1ab838e198bf45c

--- core/definitions.mk
+++ core/definitions.mk
@@ -2191,7 +2191,7 @@ endef
 # Call jack
 #
 define call-jack
- JACK_VERSION=$(PRIVATE_JACK_VERSION) $(JACK) $(DEFAULT_JACK_EXTRA_ARGS)
+ JACK_VERSION=$(PRIVATE_JACK_VERSION) flock $(BUILD_DROIDDOC) $(JACK) $(DEFAULT_JACK_EXTRA_ARGS)
 endef
 
 # Common definition to invoke javac on the host and target.
@@ -2332,6 +2332,7 @@ $(call call-jack) \
     $(if $(PRIVATE_JACK_PROGUARD_FLAGS),--config-proguard $@.flags) \
     $$tmpEcjArg \
     || ( rm -rf $(PRIVATE_CLASSES_JACK); exit 41 )
+$(hide) $(JACK)-admin server-gc
 $(hide) mv $(PRIVATE_JACK_INTERMEDIATES_DIR)/classes*.dex $(dir $@)
 $(hide) rm -f $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list
 $(if $(PRIVATE_EXTRA_JAR_ARGS),$(hide) rm -rf $@.res.tmp)
@@ -2375,6 +2376,7 @@ $(hide) if [ -s $@.java-source-list-uniq ] ; then \
 	    $(if $(PRIVATE_JACK_INCREMENTAL_DIR),--incremental-folder $(PRIVATE_JACK_INCREMENTAL_DIR)) \
 	    @$@.java-source-list-uniq; \
 fi
+$(hide) $(JACK)-admin server-gc
 touch $@
 endef
 
@@ -2394,6 +2396,7 @@ define transform-jar-to-jack
 	    --import $< \
 	    --import-resource $@.tmpjill.res \
 	    --output-jack $@
+	$(hide) $(JACK)-admin server-gc
 	$(hide) rm -rf $@.tmpjill.res
 endef
 
@@ -2515,6 +2518,7 @@ $(call call-jack) \
     $(if $(PRIVATE_JACK_PROGUARD_FLAGS),--config-proguard $@.flags) \
     $$tmpEcjArg \
     || ( rm -f $@ ; exit 41 )
+$(hide) $(JACK)-admin server-gc
 $(hide) rm -f $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list
 $(if $(PRIVATE_EXTRA_JAR_ARGS),$(hide) rm -rf $@.res.tmp)
 $(hide) mv $(PRIVATE_JACK_INTERMEDIATES_DIR)/java-source-list-uniq $(PRIVATE_JACK_INTERMEDIATES_DIR).java-source-list
