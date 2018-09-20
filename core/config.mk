58e2c07c7b0276304d367e84997004931d2122ea
mosimchah <mosimchah@gmail.com>
2018-09-20 11:18:11 -0400

    build: further AICP-ify
    Change-Id: I987062977bcd1c6db65dee39e5bbfc4245a31849

--- core/config.mk
+++ core/config.mk
@@ -229,8 +229,8 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
-include vendor/aicp/config/BoardConfigLineage.mk
+ifneq ($(AICP_BUILD),)
+include vendor/aicp/config/BoardConfigAicp.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1151,10 +1151,10 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(AICP_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/aicp/sepolicy/common/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
