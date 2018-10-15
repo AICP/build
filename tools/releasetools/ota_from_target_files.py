2ea17f2c05c706339ba086fe33b5573e5e32fe45
mosimchah <mosimchah@gmail.com>
2018-10-15 01:06:31 -0400

    Build: Remove ro.product.model from flash info
    This breaks unified devices
    
       ERROR: couldn't find ro.product.model in build.prop
    
    Change-Id: If101f31042fbe7a486435e9f7878c3773b6ccb15

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -858,10 +858,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("   Compiled: %s"%(build));
 
   device = target_info.GetBuildProp("ro.product.device")
-  if target_info.GetBuildProp("ro.product.model") is not None:
-    model = target_info.GetBuildProp("ro.product.model")
-    script.Print("   Device: %s (%s)"%(model, device));
-  else:
     script.Print("   Device: %s"%(device));
   script.Print("*********************************************");
 
