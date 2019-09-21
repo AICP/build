4d5a32c24bcd1fcf29755997771a8ded3cab5a0a
Sam Mortimer <sam@mortimer.me.uk>
2019-09-20 23:37:15 -0700

    build: Fix endif being included in line continuations
    * Move endif statements further down to prevent included in line
      continuations that otherwise cause build errors.
    
    * Issue introduced by:
      https://github.com/LineageOS/android_build/commit/0eb4c7fb1ec75b0b8ee2d15e7fc361dc8e0eede6#diff-d88e43b99e72d05cdd3680e51126db0eR34
    
    Change-Id: Iceac93077db424ff5145f41035be9beef0141583

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -37,6 +37,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg \
 
+endif
+
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
     system/app/messaging/messaging.apk \
