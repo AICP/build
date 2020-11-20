0e4c4529da52d2c8be3cfb11ca5a46d7a333cebc
koron393 <koron393@gmail.com>
2020-11-20 21:46:44 +0900

    soong: Add ability to enable product_shipping_api_level variant.
    Signed-off-by: koron393 <koron393@gmail.com>
    Change-Id: I763d5e6dd6cb141d89728fc915b47ca1bf013dd3

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -142,6 +142,7 @@ $(call add_json_bool, Malloc_not_svelte,                 $(call invert_bool,$(fi
 $(call add_json_bool, Malloc_not_svelte_libc32,          $(if $(MALLOC_SVELTE_FOR_LIBC32),\
                                                             $(call invert_bool,$(filter true,$(MALLOC_SVELTE_FOR_LIBC32))),\
                                                             $(call invert_bool,$(filter true,$(MALLOC_SVELTE)))))
+$(call add_json_val,  Product_shipping_api_level,        $(PRODUCT_SHIPPING_API_LEVEL))
 $(call add_json_str,  Override_rs_driver,                $(OVERRIDE_RS_DRIVER))
 
 $(call add_json_bool, UncompressPrivAppDex,              $(call invert_bool,$(filter true,$(DONT_UNCOMPRESS_PRIV_APPS_DEXS))))
