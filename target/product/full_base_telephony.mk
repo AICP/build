8ecb2e4b4a9c14bbc5e1f23993f49a5323ecc4ec
Han Wang <416810799@qq.com>
2021-06-09 09:49:54 +0200

    build: Fix PRODUCT_PROPERTY_OVERRIDES usage in full_base_telephony.mk
    Change-Id: Iac20b297ecefb6b3761c422bbe55c11fe9bc481a

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -23,7 +23,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
 
 ifeq ($(AICP_BUILD),)
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true
 endif
 
