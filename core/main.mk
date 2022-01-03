a9b2949ebabc55bd605fc771777296a87efcdb19
Julian Veit <claymore1298@gmail.com>
2022-01-03 17:10:02 +0100

    build: AICPify [2x]
    Change-Id: Ifd099e81dc67c8199799365fc07b69d1daabfcec

--- core/main.mk
+++ core/main.mk
@@ -469,7 +469,7 @@ ADDITIONAL_SYSTEM_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/lineage/build/core/main.mk
+-include vendor/aicp/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
