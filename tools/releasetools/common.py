423c42512e1551235208a0ce401e2cd161ca5f04
LuK1337 <priv.luk@gmail.com>
2022-12-04 12:29:34 +0100

    Pass private key password to avbkey during apex signing
    Change-Id: I56412a3f63475ea87803e8136f4a8740aaf9c2b4

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3547,7 +3547,7 @@ def ExtractPublicKey(cert):
   return pubkey
 
 
-def ExtractAvbPublicKey(avbtool, key):
+def ExtractAvbPublicKey(avbtool, key, key_password=None):
   """Extracts the AVB public key from the given public or private key.
 
   Args:
@@ -3558,8 +3558,11 @@ def ExtractAvbPublicKey(avbtool, key):
     The path to the extracted AVB public key file.
   """
   output = MakeTempFile(prefix='avb-', suffix='.avbpubkey')
-  RunAndCheckOutput(
-      [avbtool, 'extract_public_key', "--key", key, "--output", output])
+  args = [avbtool, 'extract_public_key', "--key", key, "--output", output]
+  if key_password:
+    args.append('--key_password')
+    args.append(key_password)
+  RunAndCheckOutput(args)
   return output
 
 
